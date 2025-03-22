344919e692d667b7d2da04caa17be54c94d58d46
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-03-22 19:47:43 -0400

    Connect adapter to recyclerView

--- app/src/main/java/com/udayrana/task_uday/MainActivity.kt
+++ app/src/main/java/com/udayrana/task_uday/MainActivity.kt
@@ -2,13 +2,63 @@ package com.udayrana.task_uday
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.udayrana.task_uday.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ClickListener {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: TaskAdapter
+    var taskList: MutableList<Task> = mutableListOf(
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+        Task("Walk dogs", false),
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        adapter = TaskAdapter(taskList, this)
+        binding.recyclerViewTaskList.adapter = adapter
+        binding.recyclerViewTaskList.layoutManager = LinearLayoutManager(this)
+        binding.recyclerViewTaskList.addItemDecoration(
+            DividerItemDecoration(
+                this,
+                LinearLayoutManager.VERTICAL
+            )
+        )
+    }
+
+    override fun deleteTask(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun selectTask(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun addTask() {
+        TODO("Not yet implemented")
     }
 }
