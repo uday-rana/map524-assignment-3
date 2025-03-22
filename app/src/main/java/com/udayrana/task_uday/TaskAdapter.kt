3e25d9bf3192906affae8e056e791fe6f4c86af9
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-03-22 19:38:25 -0400

    Create TaskAdapter class

---
+++ app/src/main/java/com/udayrana/task_uday/TaskAdapter.kt
@@ -0,0 +1,33 @@
+package com.udayrana.task_uday
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.udayrana.task_uday.databinding.TaskItemBinding
+
+class TaskAdapter(val taskList: MutableList<Task>, val clickListener: ClickListener) :
+    RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
+    inner class ViewHolder(val binding: TaskItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = TaskItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return taskList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val task = taskList[position]
+
+        holder.binding.textViewTaskDescription.text = task.description
+        
+        holder.binding.imageViewDeleteTask.setOnClickListener {
+            clickListener.deleteTask(position)
+        }
+        holder.binding.imageViewUpdateTask.setOnClickListener {
+            clickListener.updateTask(position)
+        }
+    }
+}
